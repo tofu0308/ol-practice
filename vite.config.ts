1bf67fc82fdd571a60fa65cceae627719b43e0aa
takeshi.fujii <isisaka3000@gmail.com>
2025-03-03 19:36:42 +0900

    build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/tofu0308/ol-practice',
+  base: 'ol-practice',
   plugins: [react()],
 });
